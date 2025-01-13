19a0597b261a165ad7e675f5b47e6cd244ee0aed
Greg Ells <gregells@gmail.com>
2025-01-13 18:57:42 -0500

    add function tests

--- isSameFirstLastCharacter.test.js
+++ isSameFirstLastCharacter.test.js
@@ -12,17 +12,27 @@ test("it should return true when the first and last character are the same", () 
 /* A test case for a string that does NOT have the same first and last 
    character and is 4+ characters long
 */
-test("", () => {});
+test("it should return false when the first and last character are not the same", () => {
+    expect(isSameFirstLastCharacter("abbbbbc")).toBe(false);
+});
 
 // Edge Cases
 // A test case for a string with only 2 characters that are different
-test("", () => {});
+test("it should return false for a string with only 2 characters that are different", () => {
+    expect(isSameFirstLastCharacter("ab")).toBe(false);
+});
 
 // A test case for a string with only 2 characters that are the same
-test("", () => {});
+test("it should return true for a string with only 2 characters that are the same", () => {
+    expect(isSameFirstLastCharacter("aa")).toBe(true);
+});
 
 // A test case for a string with only 1 character
-test("", () => {});
+test("it should return true for a string with only 1 character", () => {
+    expect(isSameFirstLastCharacter("a")).toBe(true);
+});
 
 // A test case for an empty string
-test("", () => {});
+test("it should return true for an empty string", () => {
+    expect(isSameFirstLastCharacter("")).toBe(true);
+});
